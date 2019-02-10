f6823989840c2abfb1c04e7ad390b90757ac016c
eko <ekoaripurnomo@gmail.com>
2019-02-10 15:59:43 +0700

    192 Extracting Route Parameters

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -7,10 +7,11 @@ class FullPost extends Component {
     state = {
         loadedPost: null
     }
-    componentDidUpdate () {        
-        if (this.props.id) {
+    componentDidMount () {
+        console.log(this.props);      
+        if (this.props.match.params.id) {
             if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {  
-                axios.get('/posts/' + this.props.id)    // load this if there is a new props
+                axios.get('/posts/' + this.props.match.params.id)    // load this if there is a new props
                     .then(response => {
                         // console.log(response);
                         this.setState({loadedPost: response.data});
